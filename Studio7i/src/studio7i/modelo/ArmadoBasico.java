1b6a17c8ff8685e1feb5bafd1e1a3a57a3399284
saeyta <saeyta@8f04dc69-4862-3f3e-caa2-54772a498981>
2012-08-18 16:23:20 +0000

    modificando clase ArmadoBasico

--- Studio7i/src/studio7i/modelo/ArmadoBasico.java
+++ Studio7i/src/studio7i/modelo/ArmadoBasico.java
@@ -1,5 +1,42 @@
 package studio7i.modelo;
 
-public class ArmadoBasico {
+import java.util.Collection;
 
+public class ArmadoBasico {
+	
+	private Collection<Sala> sala;
+	private Collection<Servicio> servicio;
+	private Collection<Instrumento> instrumento;
+	
+	public ArmadoBasico(Collection<Sala> sala, Collection<Servicio> servicio, Collection<Instrumento> instrumento) {
+		this.sala = sala;
+		this.servicio = servicio;
+		this.instrumento = instrumento;
+	}
+	
+	public ArmadoBasico() {
+		
+	}
+	
+	public Collection<Sala> getSala() {
+		return sala;
+	}
+	public void setSala(Collection<Sala> sala) {
+		this.sala = sala;
+	}
+	public Collection<Servicio> getServicio() {
+		return servicio;
+	}
+	public void setServicio(Collection<Servicio> servicio) {
+		this.servicio = servicio;
+	}
+	public Collection<Instrumento> getInstrumento() {
+		return instrumento;
+	}
+	public void setInstrumento(Collection<Instrumento> instrumento) {
+		this.instrumento = instrumento;
+	}
+	
+	
+	
 }
